091d3d9721f4c50080062a273c77a8ba0c2e06b6
youngroger64 <youngroger64@gmail.com>
2017-08-01 18:35:21 +0100

    initial commit

---
+++ features-to-server.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+import ast
+import pickle
+import struct
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("localhost", 30015))
+##DATA_SET_PATH = (r'C:\Users\r.young\Downloads\fordTestA.csv')
+##dataset = pd.read_csv(DATA_SET_PATH)
+dataset = ast.literal_eval(sys.argv[1])
+##dataset = ([[39.7485,10.7769,1224,49.0196,0.073947,624,96.1538,0,0,0,0,0,0.015875,366,0,0,1,70,2,0.77,-0.455,752,37.4937,0,646,0,0,0,1,11.5886]])
+
+    
+data_string = pickle.dumps(dataset)
+    
+
+s.sendall(data_string)
+
+prediction = s.recv(1024)
+pred = prediction[0]
+
+print  (pred)
+
+s.close()
+
+##NOTE --ENCASE INCOMING DATA WITH ([ ])  WHEN USING COMMAND LINE ARG
---
+++ server-side-prediction.py
@@ -0,0 +1,31 @@
+from sklearn import preprocessing
+import sys
+import ast
+from sklearn.externals import joblib
+import socket
+import pickle
+
+loaded_model = joblib.load(r'C:\Users\r.young\LR_model.sav')
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind(("localhost", 30015))
+s.listen(1)
+
+
+conn, addr = s.accept()
+data = conn.recv(1024)
+data = pickle.loads(data)
+for i in data:
+        
+        print(i)
+    
+result = loaded_model.predict(data)
+
+for i in result:
+        
+        print(i)
+        
+        conn.send(i)
+   
+
